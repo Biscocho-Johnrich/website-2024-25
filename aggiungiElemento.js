ea115f0fe2c27da15829839bad10f2a0233d4e2d
Biscocho-Johnrich <johnrich.biscocho@myenaip.eu>
2024-12-19 11:02:20 +0000

    Aggiornamento

---
+++ aggiungiElemento.js
@@ -0,0 +1,26 @@
+//Selezione il bottone di aggiunta
+
+const bottoneAggiungi = document.getElementById("aggiungi")
+const bottoneTogli = document.getElementById('bottoneTogli')
+const lista = document.getElementById('miaLista')
+
+console.log(lista.children.length)
+
+bottoneAggiungi.addEventListener('click', function ) {
+
+    const nuovoElemento = document.createElement('li');
+    const userInput = prompt("inserisci un testo");
+    const iconaCestino = document.createElement('span');
+
+
+    nuovoElemento.textContent = "userInput;
+    iconaCestino.textContent = " 🗑️ "
+    iconaCestino.style.cursor = "pointer";
+    
+    nuovoElemento.appendChild(iconaCestino);
+    lista.appendChild(nuovoElemento);
+
+    iconaCestino.addEventListener('click', function){
+        nuovoElemento.remove();
+    }
+})
