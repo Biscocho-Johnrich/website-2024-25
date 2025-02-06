9dc2ab226f6680925ca422ad56d2f4ad6fcca401
Biscocho-Johnrich <johnrich.biscocho@myenaip.eu>
2025-02-06 12:57:02 +0000

    Update

--- studioArray.js
+++ studioArray.js
@@ -1,7 +1,8 @@
 let elencoFrutta = ["kiwi", "mela", "nespole" ]
-let array1 = [];
+let array1 = []; vfwsq  
 console.log(elencoFrutta[0]);
 
+elencoFrutta.sort[]
 console.log("La lunghezza di elencoFrutta è "+ elencoFrutta.length);
 console.log("La lunghezza
     
@@ -9,4 +10,5 @@ let ultimoElementoFrutta = elencoFrutta[elencoFrutta.length-1];
 
 console.log(ultimoElementoFrutta)
 
-
+elencoFrutta;
+console.log(elencoFrutta)
